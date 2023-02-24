ef153d5d20124b0f1ea2ee5bf5f14e06e0a6600c
Tksi <Tksi@users.noreply.github.com>
2023-02-24 13:46:45 +0900

    chore: settings

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,7 @@
+<script lang="ts">
+	const say = (str): void => {};
+</script>
+
 <svelte:head>
 	<title>mob-next</title>
 	<meta name="robots" content="noindex nofollow" />
--- svelte.config.js
+++ svelte.config.js
@@ -9,7 +9,9 @@ const config = {
 
 	kit: {
 		adapter: adapter(),
-		'$/*': 'src/routes/*'
+		alias: {
+			'$/*': 'src/routes/*'
+		}
 	}
 };
 
